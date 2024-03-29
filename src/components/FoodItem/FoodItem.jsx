837a16af310b61281893213d626ca7fe476277a7
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-03-29 23:24:02 +0600

    commit

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import { assets } from "../../assets/assets";
-import "./FoodItem.css"
+import "./FoodItem.css";
 
 function FoodItem({ id, name, description, price, image }) {
+  const [itemCount, setItemCount] = useState(0);
+
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img src={image} alt="" className="food-item-image" />
+        {!itemCount ? (
+          <img
+            className="add"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+            <p>{itemCount}</p>
+            <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-tiem-name-rating">
--- src/context/FoodsContext.jsx
+++ src/context/FoodsContext.jsx
@@ -1,9 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from '../assets/assets';
 
 export const FoodsContext = createContext(null)
 
 const FoodsProvider = (props) => {
+
+    const [cartItems, setCartItems] = useState({})
+
+    const addToCart = (itemId) => {
+        if(!cartItems[itemId]){
+            setCartItems((prev)=>({...prev, [itemId]:1}))
+        }
+    }
+
     const contextValue = {
         food_list
     }
