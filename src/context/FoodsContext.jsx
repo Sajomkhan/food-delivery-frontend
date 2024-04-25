1f825f872b9d62b1847d3a51b10baf3a1e1d2d4f
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-04-25 23:20:01 +0600

    Update FoodsContext.jsx

--- src/context/FoodsContext.jsx
+++ src/context/FoodsContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
-// Create context 
+// Create Context 
 export const FoodsContext = createContext(null);
 
 // Context provider
